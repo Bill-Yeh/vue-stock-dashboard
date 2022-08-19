5f2b834d96ac938ccd4c7717cb5356911e714527
Bill-Yeh <lawyer112240@gmail.com>
2022-08-19 22:51:54 +0800

    左側與上方切板完成

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,12 @@
             <img src="@/assets/money.png" alt="Investment" />
           </router-link>
         </div>
+        <div class="logo-name">
+          <h1>Investment</h1>
+        </div>
+        <div class="desk-arrow">
+          <i class="bi bi-chevron-left"></i>
+        </div>
         <div class="header-bell">
           <i class="bi bi-bell-fill"></i>
         </div>
@@ -32,9 +38,6 @@
               <li class="rwd-exchange">
                 <i class="bi bi-currency-exchange"></i>
                 <span>Exchange</span>
-                <div class="rwd-item-arrow-down">
-                  <i class="bi bi-chevron-down"></i>
-                </div>
               </li>
               <li class="rwd-stock">
                 <i class="bi bi-graph-up"></i>
@@ -56,6 +59,33 @@
         </div>
       </nav>
     </header>
+    <div class="right-container">
+      <div class="top-bar">
+        <div class="top-bar-wrap">
+          <div class="top-search-function">
+            <input type="text" placeholder="Search for stock or more" />
+            <i class="bi bi-search"></i>
+          </div>
+          <div class="bell-user-info-wrap">
+            <div class="top-bell">
+              <i class="bi bi-bell-fill"></i>
+            </div>
+            <div class="top-user">
+              <i class="bi bi-person-fill"></i>
+            </div>
+            <div class="user-info">
+              <div class="user-name">
+                <span>Bill Yeh</span>
+              </div>
+              <div class="user-email">
+                <span>abc@gmail.com</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main-content"></div>
+    </div>
     <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
