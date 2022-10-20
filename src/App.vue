bb33d825dd3e866d7c8201221aac340103292ae8
Bill-Yeh <lawyer112240@gmail.com>
2022-10-20 23:25:50 +0800

    安裝vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,112 +1,50 @@
 <template>
-  <div id="app">
-    <sidebarLeft />
-    <sidebarTop />
-    <!-- <input type="checkbox" id="rwd-switch" />
-    <header class="rwd-header">
-      <div class="header-wrap">
-        <label for="rwd-switch" class="header_hb">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-        <div class="header-logo">
-          <router-link to="/">
-            <img src="@/assets/money.png" alt="Investment" />
-          </router-link>
-        </div>
-        <div class="logo-name">
-          <h1>Investment</h1>
-        </div>
-        <div class="desk-arrow">
-          <i class="bi bi-chevron-left"></i>
-        </div>
-        <div class="header-bell">
-          <i class="bi bi-bell-fill"></i>
-        </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
       </div>
-      <nav class="rwd-main-nav">
-        <div class="rwd-main-nav-wrap">
-          <div class="search-function">
-            <input type="text" placeholder="Search for stock or more" />
-            <i class="bi bi-search"></i>
-          </div>
-          <div class="rwd-nav-item">
-            <ul class="item-list">
-              <li class="rwd-home">
-                <i class="bi bi-house-door-fill"></i>
-                <span>Home</span>
-              </li>
-              <li class="rwd-exchange">
-                <i class="bi bi-currency-exchange"></i>
-                <span>Exchange</span>
-              </li>
-              <li class="rwd-stock">
-                <i class="bi bi-graph-up"></i>
-                <span>Stock</span>
-              </li>
-              <li class="rwd-crypto">
-                <i class="bi bi-currency-bitcoin"></i>
-                <span>Crypto</span>
-              </li>
-              <li class="rwd-member">
-                <i class="bi bi-person-fill"></i>
-                <span>Member</span>
-                <div class="rwd-item-arrow-down">
-                  <i class="bi bi-chevron-down"></i>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
-    <div class="right-container">
-      <div class="top-bar">
-        <div class="top-bar-wrap">
-          <div class="top-search-function">
-            <input type="text" placeholder="Search for stock or more" />
-            <i class="bi bi-search"></i>
-          </div>
-          <div class="bell-user-info-wrap">
-            <div class="top-bell">
-              <i class="bi bi-bell-fill"></i>
-            </div>
-            <div class="top-user">
-              <i class="bi bi-person-fill"></i>
-            </div>
-            <div class="user-info">
-              <div class="user-name">
-                <span>Bill Yeh</span>
-              </div>
-              <div class="user-email">
-                <span>abc@gmail.com</span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="main-content"></div>
-    </div> -->
-    <!-- <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav> -->
-    <router-view />
-  </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import sidebarLeft from "@/components/sidebar_left.vue";
-import sidebarTop from "@/components/sidebar_top.vue";
 export default {
-  components: {
-    sidebarLeft,
-    sidebarTop,
-  },
+  name: "App",
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style lang="scss">
-@import "assets/scss/main.scss";
-</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/scss/main.scss";
+import vuetify from "./plugins/vuetify";
 // import "./assets/tailwind.css";
 
 Vue.config.productionTip = false;
@@ -10,5 +11,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
