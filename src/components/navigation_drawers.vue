b63e406557bf215d3389703f8c880c048e577822
Bill-Yeh <lawyer112240@gmail.com>
2022-10-23 23:38:37 +0800

    製作navbar

--- src/components/navigation_drawers.vue
+++ src/components/navigation_drawers.vue
@@ -1,16 +1,42 @@
 <template>
-  <v-card
-    class="overflow-hidden"
-    height="400"
-    width="344"
-  >
-    <v-system-bar color="deep-purple darken-3"></v-system-bar>
+  <v-card>
+    <v-toolbar flat dark rounded="0" extended>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Stock Dashboard</v-toolbar-title>
 
-    <v-app-bar
-      color="deep-purple accent-4"
-      dark
-      prominent
-    >
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer app v-model="drawer" dark absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="white--text">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- <v-app-bar color="deep-purple accent-4" dark prominent>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>My files</v-toolbar-title>
@@ -28,43 +54,23 @@
       <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
-    </v-app-bar>
-
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      bottom
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-title>Foo</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Bar</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Fizz</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-title>Buzz</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-card-text>
-      The navigation drawer will appear from the bottom on smaller size screens.
-    </v-card-text>
+    </v-app-bar> -->
   </v-card>
 </template>
+
+<script>
+/* eslint-disable */
+  export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+  /* eslint-disable */
+</script>
